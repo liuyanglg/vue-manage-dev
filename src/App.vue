9cf9d9f981720377cda3152d5d7dd96528ed1996
liuyanglg@outlook.com <j00jpg809>
2018-02-10 22:35:12 +0800

    vue learn

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="fillcontain">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="less">
+@import "./style/common";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,15 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
+
+const routes = [
+  {
+    path: '/',
+    component: login
+  }
+]
 export default new Router({
-  routes: [
-  ]
+  routes,
+  strict: process.env.NODE_ENV !== 'production',
 })
